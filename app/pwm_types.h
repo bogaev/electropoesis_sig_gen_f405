b4b4ddd5edc107780b86d97409a1c155b96a6488
Sergey Bogaev <bogaev.s@gmail.com>
2023-09-14 13:37:10 +0700

    feat: Add separate start button that switch power relay

--- app/pwm_types.h
+++ app/pwm_types.h
@@ -29,8 +29,12 @@ typedef enum {
 //} enSignalParams;
 
 typedef enum {
-  COM_MSG_CHANGE_PARAM = 0,
-  COM_MSG_COMMIT
+  COM_MSG_PAUSE_ALL = 0,
+  COM_MSG_CHECK_MCU_READY,
+  COM_MSG_CHANGE_PARAM,
+  COM_MSG_COMMIT,
+  COM_MSG_RELAY_ON,
+  COM_MSG_RELAY_OFF
 } tdMessageType;
 
 typedef struct {
